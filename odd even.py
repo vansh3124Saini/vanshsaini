a079a051718a11c6e84acc2769a8f35a92e7e416
vansh3124Saini <vanshsaini312@gmail.com>
2025-03-08 23:37:02 +0530

    Create odd even.py

---
+++ odd even.py
@@ -0,0 +1,11 @@
+a = int(input("enter the value of a"))
+b = int(input("enter the value of b"))
+c = int(input("enter the value of c"))
+
+
+if(a>b and a>c):
+    print("a is greater")
+elif(b>a and b>c):
+    print("b is greater")
+else:
+    print("c is greater")104030
