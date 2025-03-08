b69db74672bba1b5323313e1341e50b216ff8d3a
vansh3124Saini <vanshsaini312@gmail.com>
2025-03-08 23:27:41 +0530

    Update varible.py

--- varible.py
+++ varible.py
@@ -1,5 +1,11 @@
-a = 2
+a = int(input("enter the value of a"))
+b = int(input("enter the value of b"))
+c = int(input("enter the value of c"))
+d = int(input("enter the value of d"))
+e = int(input("enter the value of e"))
+f = int(input("enter the value of f"))
+g = int(input("enter the value of g"))
 
-b = 3
 
-print (a+b)
+output = a + b + c + d + e + f + g
+print(output)
