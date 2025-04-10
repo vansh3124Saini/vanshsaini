4d080c7b9d212496c8df11fe63cc1826883e72f4
vansh3124Saini <vanshsaini312@gmail.com>
2025-04-10 19:47:17 +0530

    Create 05_squaring number.py

---
+++ 05_squaring number.py
@@ -0,0 +1,5 @@
+a = int(input("enter the number:"))
+
+print("the square of the number is",a**2) # this is valid
+print("the square of the number is",a*2) # this is valid
+print("the square of the number is",a^2) #this is invalid
